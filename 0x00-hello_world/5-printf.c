6232732e2309e3ed0e361a2bbb0f23abf1f329c9
Timimah <timimah0117@gmail.com>
2023-02-20 07:00:29 -0800

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/**
+ * A program that prints a phrase and 
+ * returns 0
+ */
+#include <stdio.h>
+
+/**
+ * main - prints a string
+ *
+ * Return - returns zero after the print code runs
+ */
+int main(void)
+{
+	printf(%s"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
