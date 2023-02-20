3290d64c22146632292853212c0fdbc101ecf5b6
Timimah <timimah0117@gmail.com>
2023-02-20 06:31:58 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - prints "Programming is like building a
  *  multilingual puzzle"
- *  Returns 0
+ *  Return: Returns zero
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
