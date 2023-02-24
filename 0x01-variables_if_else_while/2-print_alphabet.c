06487c200a80ea8c4247147d48139f4feaa37295
Timimah <timimah0117@gmail.com>
2023-02-24 01:24:20 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet from a - z
+ * in lowercase
+ * Return: main returns zero when the
+ * program is successful
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	putchar("The alphabeths are ");
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	return (0);
+}
