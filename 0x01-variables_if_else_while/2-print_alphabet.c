1100eb045157d90efc77105c53cac643a9581a83
Timimah <timimah0117@gmail.com>
2023-02-24 02:12:05 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,9 +10,9 @@ int main(void)
 {
 	char alpha;
 
-	for (alpha = 'a'; alpha <= 'z'; alpha ++)
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-		putchar(alpha);
+		putchar(alpha," ");
 	}
 	return (0);
 }
