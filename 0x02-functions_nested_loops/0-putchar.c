bcede01244b10db90984c49b3c4056e6b03fd487
Timimah <timimah0117@gmail.com>
2023-02-27 02:05:20 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints '_putchar', followed by a new line
+ * Return: main returns 0 (i.e. success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
